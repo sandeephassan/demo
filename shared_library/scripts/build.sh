efe918dbce329d65c50030597cf29801eb2975c1
shkanadamane28 <shkanadamane28@gmail.com>
2026-07-08 00:15:19 +0530

    update

--- shared_library/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-PROJECT_DIR="$1"
-BUILD_DIR="$2"
-mkdir -p "$PROJECT_DIR/$BUILD_DIR"
-cd "$PROJECT_DIR/$BUILD_DIR"
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake
-cmake --build .
--- shared_library/scripts/prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-echo "led-shared: install packages (script)"
-apt-get update || true
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cmake clang-format gcc-arm-none-eabi binutils-arm-none-eabi build-essential || true
